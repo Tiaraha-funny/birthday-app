b2e7fa2712d0d9ded9949e7669b98c2e3ab6e8bf
Peta Jeannie <peta.jea@onja.org>
2020-09-15 15:48:34 +1030

    doing the edit people's birthday function with form html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// const { async } = require("regenerator-runtime");
 
 const main = document.querySelector("main");
 
@@ -21,10 +22,10 @@ async function givePeopleBirthdayList() {
           <li class="names">${birth.lastName} ${birth.firstName}</li>
           <li class="">${birth.birthday}</li>
           <li class="edit">
-            <button type="button" name="edit" class="edit rounded border-0 bg-white "><svg class="w-6 h-6" width="20px" height="20px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+            <button type="button" name="edit" class="edit"><svg class="edit" width="20px" height="20px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
           </li>
           <li class="delete">
-            <button type="button" class="delete rounded border-0 bg-white "><svg class="w-6 h-6"  width="20px" height="20px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+            <button type="button" class="delete"><svg class="delete"  width="20px" height="20px" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
           </button>
           </li>
         </ul>
@@ -34,15 +35,15 @@ async function givePeopleBirthdayList() {
   main.innerHTML = html;
 }
 
-const handleClick = e => {
+const handleClick = (e) => {
   console.log("The edit is clicked");
-  if(e.target.closest("button.edit")) {
+  if (e.target.closest("button.edit")) {
     console.log("You can edit here");
     const parent = e.target.closest("ul");
     const id = parent.dataset.id;
     editPersonBirthday(id);
   }
-  if(e.target.closest("button.delete")) {
+  if (e.target.closest("button.delete")) {
     console.log("You are able to delete");
     const parent = e.target.closest("ul");
     const id = parent.dataset.id;
@@ -50,54 +51,45 @@ const handleClick = e => {
   }
 };
 
-const editPersonBirthday = (id) => {
+async function editPersonBirthday(id) {
   console.log("Edit is clicked");
-  
-  let personToEdit = birthPerson.find((person) => person.id === id);
+
+  const editBirthday = await fetchPeople();
+
+  const personToEdit = editBirthday.find((person) => person.id === id);
   console.log(personToEdit);
 
   return new Promise(function (resolve) {
     const newForm = document.createElement("form");
     newForm.classList.add("person");
-    newForm.insertAdjacentElement(
-      "afterbegin",
-      `
-      <h1>Is this your partner?</h1>
-      <fieldset>
-        <label>Last Name</labe>
-        <input type="text" name="lastName" id="lastname" value="${personToEdit.lastName}">
-      </fieldset>
-      <fieldset>
-        <label>First name</labe>
-        <input type="text" name="firstName" id="firstname" value="${personToEdit.firstName}">
-      </fieldset>
-      <fieldset>
-        <label>Job title</labe>
-        <input type="text" name="jobTitle" id="title" value="${personToEdit.jobTitle}">
-      </fieldset>
-      <fieldset>
-        <label>Job area</labe>
-        <input type="text" name="jobArea" id="jobarea" value="${personToEdit.jobArea}">
-      </fieldset>
-      <fieldset>
-        <label>Phone number</labe>
-        <input type="text" name="phone" id="number" value="${personToEdit.phone}">
-      </fieldset>
-      <div class="buttons">
-        <button type="submit">
-          Save
-        </button>
-  `
-    );
+    const editHtml= `
+    <div class="form">
+          <h1>Do you want to edit something?</h1>
+            <label>Last Name</labe><br>
+            <input type="text" name="lastName" id="lastname" value="${personToEdit.lastName}">
+            <label>First name</labe><br>
+            <input type="text" name="firstName" id="firstname" value="${personToEdit.firstName}">
+            <label>Birthday</labe><br>
+            <input type="text" name="birthday" id="birthday" value="${personToEdit.birthday}">
+          <div class="buttons">
+            <button type="submit">
+              Save
+            </button>
+            <button type="button" name="cancel">
+              cancel
+            </button>
+          </div>
+        </div>
+    `
+    newForm.insertAdjacentHTML("afterbegin", editHtml);
 
-    newForm.addEventListener("submit", e => {
+    newForm.addEventListener("submit", (e) => {
       e.preventDefault();
       resolve();
     });
-    
+
     resolve(document.body.appendChild(newForm));
     newForm.classList.add("open");
-
   });
 };
 
