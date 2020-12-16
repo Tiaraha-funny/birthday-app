328a291b1ba3522e94b162d055a9dd2670465897
Peta Jeannie <peta.jea@onja.org>
2020-12-16 11:51:34 -0600

    changed the URL API so that we can get multiple images in the birthday states

--- BirthdayApp/script.js
+++ BirthdayApp/script.js
@@ -10,7 +10,7 @@ import { handleClick } from "./click.js";
 
 //Fetch all the people in the list
 const peps =
-  "https://gist.githubusercontent.com/Pinois/e1c72b75917985dc77f5c808e876b67f/raw/93debb7463fbaaec29622221b8f9e719bd5b119f/birthdayPeople.json";
+  "https://gist.githubusercontent.com/Pinois/e1c72b75917985dc77f5c808e876b67f/raw/b17e08696906abeaac8bc260f57738eaa3f6abb1/birthdayPeople.json";
 
 //Drag the elements from the html
 const main = document.querySelector("main");
